c2cc44fab6964e191179279623ce033d7c95f025
brown_sweet <455764041@qq.com>
2026-05-15 10:14:09 +0800

    first commit

---
+++ src/Test.php
@@ -0,0 +1,14 @@
+<?php
+namespace Brown\Longbridge;
+/**
+ *   Author:Brown
+ *   Email: 455764041@qq.com
+ *   Time: 2026-05-15 10:08
+ */
+class Test
+{
+    public function index()
+    {
+        echo 1;
+    }
+}
